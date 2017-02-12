47b9e7c4f3ac2ca5d868a0a7dce0e7f1fe20c7b3
Jaime Aranaz <jaimeat1@gmail.com>
2017-02-12 11:53:57 +0100

    FLP-1 Switch between source and records view

--- Flip/FLPMainViewController.m
+++ Flip/FLPMainViewController.m
@@ -10,11 +10,17 @@
 
 #import "FLPTitleLetterView.h"
 
+#define kSlideAnimationDuration 0.3
+
 @interface FLPMainViewController () <SourceViewDelegate, RecordsViewDelegate>
 
 @property (weak, nonatomic) IBOutlet TitleView *titleView;
 @property (weak, nonatomic) IBOutlet RecordsView *recordsView;
 @property (weak, nonatomic) IBOutlet SourceView *sourceView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *recordsLeading;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *sourceLeading;
+
+@property (nonatomic) CGFloat screenWidth;
 
 @end
 
@@ -22,13 +28,27 @@
 
 #pragma mark - Lifecycle methods
 
+- (CGFloat)screenWidth
+{
+    if (_screenWidth == 0) {
+        _screenWidth = CGRectGetWidth([UIScreen mainScreen].bounds);
+    }
+    
+    return _screenWidth;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.titleView startAnimation];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
     self.recordsView.delegate = self;
     self.sourceView.delegate = self;
-    [self.titleView startAnimation];
 }
 
 - (void)viewDidDisappear:(BOOL)animated
@@ -37,11 +57,18 @@
     [self.titleView stopAnimation];
 }
 
+- (void)updateViewConstraints
+{
+    [super updateViewConstraints];
+    
+    [self showSourceView:NO];
+}
+
 #pragma mark - RecordsViewDelegate methods
 
 - (void)didSelectStartGame
 {
-    // TODO: implement
+    [self showSourceView:YES];
 }
 
 #pragma mark - SourceViewDelegate methods
@@ -63,7 +90,7 @@
 
 - (void)didSelectRecords
 {
-    // TODO: implement
+    [self showRecordsView];
 }
 
 #pragma mark - MainViewControllerDelegate methods
@@ -71,6 +98,44 @@
 - (void)showRecordsWithSmall:(NSTimeInterval)small medium:(NSTimeInterval)medium big:(NSTimeInterval)big
 {
     // TODO: implement
+}
+
+#pragma mark - Private methods
+
+- (void)showRecordsView
+{
+    [UIView animateWithDuration:kSlideAnimationDuration
+                     animations:^{
+                         [self updateConstraintsToShowRecordsView];
+                         [self.view layoutIfNeeded];
+                     }];
+}
+
+- (void)showSourceView:(BOOL)animated
+{
+    if (animated) {
+        
+        [UIView animateWithDuration:kSlideAnimationDuration
+                         animations:^{
+                             [self updateConstraintsToShowSourceView];
+                             [self.view layoutIfNeeded];
+                         }];
+    } else {
+
+        [self updateConstraintsToShowSourceView];
+    }
+}
+
+- (void)updateConstraintsToShowRecordsView
+{
+    self.recordsLeading.constant = 0;
+    self.sourceLeading.constant = self.screenWidth;
+}
+
+- (void)updateConstraintsToShowSourceView
+{
+    self.sourceLeading.constant = 0;
+    self.recordsLeading.constant = -self.screenWidth;
 }
 
 @end
