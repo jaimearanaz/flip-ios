adcf27a287071b1b75ca4b1e5d7e6427491c4360
Jaime Aranaz <jaimeat1@gmail.com>
2017-02-17 08:07:44 +0100

    FLP-2 Build xib for grid

--- Flip/FLPNewGridViewController.m
+++ Flip/FLPNewGridViewController.m
@@ -8,17 +8,50 @@
 
 #import "FLPNewGridViewController.h"
 
-@interface FLPNewGridViewController ()
+@interface FLPNewGridViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
 @implementation FLPNewGridViewController
+
+#pragma mark - Action methods
+
+- (IBAction)didSelectExit:(id)sender
+{
+    [self confirmExit];
+}
+
+#pragma mark - UICollectionViewDelegate methods
+
+#pragma mark - UICollectionViewDataSource methods
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [[UICollectionViewCell alloc] init];
+}
+
+#pragma mark - UICollectionViewDelegateFlowLayout methods
 
 #pragma mark - NewGridViewControllerDelegate methods
 
 - (void)showPicturesWithPictures:(NSArray<NSString *> *)pictures
 {
     // TODO: implement
+}
+
+#pragma mark - Private methods
+
+- (void)confirmExit
+{
+    
 }
 
 @end
